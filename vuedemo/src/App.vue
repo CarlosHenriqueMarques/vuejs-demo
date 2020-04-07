460f53159222a5e8e5d92ffc66515aba6f21af76
Carlos Maia <carlos.marques89@gmail.com>
2020-04-07 14:57:09 -0400

    Interando em uma table a lista de fotos

--- vuedemo/src/App.vue
+++ vuedemo/src/App.vue
@@ -1,7 +1,15 @@
 <template>
 <div>
-  <h1>{{titulo}}</h1>
-<img v-bind:src="foto.url" v-bind:alt="foto.titulo">
+  <h1 v-text="titulo"></h1>
+   
+   
+   <ul>
+     <li v-for="foto of fotos">
+        <img :src="foto.url" :alt="foto.titulo  ">
+     </li>
+   </ul>
+   
+   
 </div>
 </template>
 
@@ -10,10 +18,16 @@ export default {
   data(){
     return{
       titulo: 'VueJs Demo',
-      foto : {
-        url :  "https://www.clubeparacachorros.com.br/wp-content/uploads/2018/07/cachorros-mais-fofos-do-mundo-lulu-da-pomerania-filhote.jpg",
-        titulo : "cachorro fofo"
-      }
+      fotos : [ 
+        {
+          url :  "https://www.clubeparacachorros.com.br/wp-content/uploads/2018/07/cachorros-mais-fofos-do-mundo-lulu-da-pomerania-filhote.jpg",
+          titulo : "cachorro fofo"
+        },
+        {
+          url :  "https://www.clubeparacachorros.com.br/wp-content/uploads/2018/07/cachorros-mais-fofos-do-mundo-lulu-da-pomerania-filhote.jpg",
+          titulo : "cachorro fofo2"
+        }
+      ] 
     }
   }
 }
