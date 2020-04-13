962f98c4fca461233e0037539dcd57cfa095d6d4
Carlos Maia <carlos.marques89@gmail.com>
2020-04-13 14:03:59 -0400

    configurando rotas para uilizar o singlepage application

--- vuedemo/src/App.vue
+++ vuedemo/src/App.vue
@@ -1,91 +1,19 @@
 <template>
   <div class="corpo">
-    <h1 class="centralizado">{{ titulo }}</h1>
-
-    <input
-      type="search"
-      class="filtro"
-      v-on:input="filtro = $event.target.value"
-      placeholder="filtre pelo título da foto"
-    />
-
-    <ul class="lista-fotos">
-      <li class="lista-fotos-item" v-for="foto of fotosComFiltro">
-        <meu-painel :titulo="foto.titulo">
-          <img class="imagem-responsiva" :src="foto.url" :alt="foto.titulo" />
-        </meu-painel>
-      </li>
-    </ul>
+   
   </div>
 </template>
 
 <script>
-import Painel from "./components/shared/painel/Painel.vue";
+ export default{
 
-export default {
-  components: {
-    "meu-painel": Painel
-  },
-
-  data() {
-    return {
-      titulo: "VueJs Demo",
-
-      fotos: [],
-
-      filtro: ""
-    };
-  },
-
-  computed: {
-    //Método para filtrar as imagens chamada v-on do input
-    fotosComFiltro() {
-      if (this.filtro) {
-        let exp = new RegExp(this.filtro.trim(), "i");
-        return this.fotos.filter(foto => exp.test(foto.titulo));
-      } else {
-        return this.fotos;
-      }
-    }
-  },
-
-  //Evento na criação da tela
-  created() {
-    this.$http
-      .get("http://localhost:3000/v1/fotos")
-      .then(res => res.json())
-      .then(
-        fotos => (this.fotos = fotos),
-        err => console.log(err)
-      );
-  }
-};
+ }
 </script>
 
 <style>
-.centralizado {
-  text-align: center;
-}
-
 .corpo {
   font-family: Helvetica, sans-serif;
   margin: 0 auto;
   width: 96%;
-}
-
-.lista-fotos {
-  list-style: none;
-}
-
-.lista-fotos .lista-fotos-item {
-  display: inline-block;
-}
-.imagem-responsiva {
-  width: 100%;
-}
-
-.filtro {
-  display: block;
-  width: 100%;
 }
 </style>
--- vuedemo/src/components/shared/painel/Painel.vue
+++ vuedemo/src/components/shared/painel/Painel.vue
@@ -1,12 +1,13 @@
 <template>
 
   <div class="painel">
-
+    <!-- Se o double click for diferente aplicar o valor diferente-->
     <h2 class="painel-titulo" @dblclick="visivel = !visivel">{{ titulo }}</h2>
-    <div class="painel-conteudo" v-show="visivel">
-      <slot></slot>
-    </div>
-
+    <transition name="painel-fade">
+      <div class="painel-conteudo" v-show="visivel">
+        <slot></slot>
+      </div>
+    </transition>
   </div>
 
 </template>
@@ -54,5 +55,12 @@ export default {
   *  {
     box-shadow: 5px 5px 5px;
   }
+  .painel-fade-enter, .painel-fade-leave-active{
+    opacity: 0;
+  }
+    
+    .painel-fade-enter-active, .painel-fade-leave-active{
+      transition: .4s;
+    }
 
 </style>
